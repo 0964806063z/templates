802453ffa60b17f65a8453c8163827f951093d3e
DESKTOP-DGE1FFR\Le Nguyen <lequynguyenk57@gmail.com>
2018-06-20 16:53:32 +0700

    note

--- src/html/data/interior.js
+++ src/html/data/interior.js
@@ -1,9 +1,11 @@
 module.exports = {
-  title: 'Inerior',
-  intro: 'Cloud Overview',
-  intro2: 'Private Cloud',
-  intro3: 'Dedicated cloud',
-  intro4: 'Bare Metal',
+
   titlebanner: 'Cloud',
-  introbanner: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus.'
+  introbanner: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus.',
+  'note':[
+    {intro:"Cloud Overview"},
+    {intro: "Private Cloud"},
+    {intro: "Dedicated cloud"},
+    {intro: "Bare Metal"}
+  ]
 }
--- src/html/data/leadership.js
+++ src/html/data/leadership.js
@@ -1,10 +1,11 @@
 module.exports = {
-  title: 'Inerior',
-  intro: 'Company Profile',
-  intro2: 'Leadership',
-  intro3: 'News',
-  intro4: 'Technology Parners',
-  intro5: 'Careers',
+  'note':[
+    {intro:"Company Profile"},
+    {intro: "Leadership"},
+    {intro: "Technology Parners"},
+    {intro: "News"},
+    {intro: "Careers"}
+  ],
   titlebanner: 'MTL1',
   introbanner: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus.eStruxture is a network and cloud-neutral data center company',
   'center':[
