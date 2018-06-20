e82be41d0ddc80e842654098734fda103c30a98b
DESKTOP-DGE1FFR\Le Nguyen <lequynguyenk57@gmail.com>
2018-06-20 15:29:26 +0700

    20/6

--- src/html/data/leadership.js
+++ src/html/data/leadership.js
@@ -6,5 +6,12 @@ module.exports = {
   intro4: 'Technology Parners',
   intro5: 'Careers',
   titlebanner: 'MTL1',
-  introbanner: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus.eStruxture is a network and cloud-neutral data center company'
+  introbanner: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus.eStruxture is a network and cloud-neutral data center company',
+  'center':[
+    {img:"../images/1.jpg",name:"Todd Coleman", job:"President and CEO",},
+    {img:"../images/2.jpg",name:"Marc Belanger", job:"Cheif Financial Officer",},
+    {img:"../images/3.jpg",name:"John Savageu", job:"Vice President, Operationsand Engieneering",},
+    {img:"../images/4.jpg",name:"Billy Krassakopoulos", job:"Vice President Montreal Market",},
+    {img:"../images/1.jpg",name:"Strahan McCarten", job:"Senior Vice President Product & Strategy",}
+  ]
 }
