8eedcfddeff7feadf0e9230bf8d4607403eb3cdb
DESKTOP-DGE1FFR\Le Nguyen <lequynguyenk57@gmail.com>
2018-06-21 09:36:14 +0700

    banner

--- src/html/data/index.js
+++ src/html/data/index.js
@@ -1,4 +1,7 @@
 module.exports = {
-  titlebanner: 'High-density power where you want it-right here in Canada',
+  titlebanner: ' power ',
+  wbanner1: 'High-density',
+  wbanner2: 'where you want it-right here in',
+  tittlebanner1:' Canada',
   introbanner: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus.eStruxture is a network and cloud-neutral data center company'
 }
